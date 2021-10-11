e2c1f3f625463c349f9b412650eb2257b07a5374
gokul <you@example.com>
2021-10-11 09:50:15 +0530

    gokul

--- task.js
+++ task.js
@@ -1,4 +1,3 @@
-
 var options = ["Angular", "React JS", "Vue JS", "C Language", "C++", "Java", "Javascript", "Objective-C", "PHP", "Python", "SQL"];
 var selectedoptions = [];
 
@@ -6,41 +5,47 @@ document.addEventListener(
   "DOMContentLoaded",
   function () {
     createOptions();
-    loadSelectedList();
+    getSelectedList();
   },
   false
 );
 
 function createOptions() {
+
   var searchOption = document.getElementById("left_searchbox").value.toUpperCase();
   const name = document.getElementsByClassName("left_options")[0];
-  name.innerHTML = ""
+  name.innerHTML = "";
+
   if (searchOption === "" || searchOption === undefined) {
+
     options.sort().forEach(function (value, i) {
       optionBox(value, i);
-
     });
+
   }
   else {
+
     const curentvalue = options.filter(val => val.toUpperCase().indexOf(searchOption.trim()) > -1)
     if (curentvalue.length) {
+
       curentvalue.forEach(function (value, i) {
         optionBox(value, i);
       });
     } else {
+
       let display = `<div class="nodata"><i class="fas fa-search fa-8x"></i><p>Search Not Found</p></div>`;
       name.innerHTML = display
     }
   }
   customDrag();
-
 }
 
-
 function optionBox(value, i) {
+
   const id = value.split(" ").join("").replace(/[^0-9a-z]/gi, '');
-  const name = document.getElementById("leftoption");
-  var div = document.createElement('div');
+  const name = document.getElementById("left_options");
+  let div = document.createElement('div');
+
   div.setAttribute('id', `optionbox${id}`);
   div.setAttribute('class', "draggable optioncontainer")
   div.innerHTML = ` <div  class='optionbox'><p>${value}</p><i class="fas fa-grip-vertical" ></i></div> <i class="fas fa-trash-alt fa-lg deletebtn" onclick='deleteoptions("optionbox${id}","${value}")'></i> `;
@@ -48,75 +53,8 @@ function optionBox(value, i) {
   name.appendChild(div);
 }
 
-function customDrag() {
-  const draggables = document.querySelectorAll('.optioncontainer')
-  const containers = document.querySelectorAll('#right_options')
-  const dropzones = document.querySelectorAll('#dropzone')
-  draggables.forEach(draggable => {
-
-    draggable.addEventListener('dragstart', (event) => {
-      event
-        .dataTransfer
-        .setData('text/plain', event.target.id);
-      draggable.classList.add('dragging')
-
-    })
-
-    draggable.addEventListener('dragend', () => {
-      draggable.classList.remove('dragging')
-    })
-  })
-  containers.forEach(container => {
-    let afterElement;
-    container.addEventListener('dragover', e => {
-      e.preventDefault()
-
-    })
-    container.addEventListener('drop', e => {
-      const draggable = document.querySelector('.dragging')
-      const afterElement = getDragAfterElement(container, e.clientY)
-
-      if (afterElement == null) {
-        container.appendChild(draggable)
-      } else {
-        container.insertBefore(draggable, afterElement)
-      }
-    });
-
-  })
-  dropzones.forEach(dropzone => {
-    dropzone.addEventListener('dragover', e => {
-      e.preventDefault()
-
-    })
-    dropzone.addEventListener('drop', event => {
-      const id = event
-        .dataTransfer
-        .getData('text');
-      const draggableElement = document.getElementById(id);
-
-      const droparea = document.getElementById("right_options")
-      droparea.prepend(draggableElement);
-      event.dataTransfer.clearData();
-
-    })
-  })
-
-  function getDragAfterElement(container, y) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-    return draggableElements.reduce((closest, child) => {
-      const box = child.getBoundingClientRect()
-      const offset = y - box.top - box.height / 2
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child }
-      } else {
-        return closest
-      }
-    }, { offset: Number.NEGATIVE_INFINITY }).element
-  }
-}
-
 function deleteoptions(id, value) {
+
   myobj = document.querySelectorAll(`#${id}`)[0];
   myobj.parentNode.removeChild(myobj);
   options.push(value)
@@ -124,15 +62,17 @@ function deleteoptions(id, value) {
   customDrag();
 }
 
-function loadSelectedList() {
+function getSelectedList() {
   const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
   const list = document.getElementById("right_options")
+
   const config = {
     attributes: true,
     childList: true,
     characterData: true,
     subtree: true,
   };
+
   const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (mutation.type === "childList") {
@@ -149,10 +89,79 @@ function loadSelectedList() {
         })
       }
     });
+
     const dropzoneId = document.getElementById("dropzone");
     selectedoptions.length > 0 ?
-      (dropzoneId.style.height = "100px", dropzoneId.style.background = "white", document.getElementsByClassName("dropicon")[0].style.display = "none")
-      : (dropzoneId.style.height = "600px", dropzoneId.style.background = "E5E5E5", document.getElementsByClassName("dropicon")[0].style.display = "block");
+      (document.getElementsByClassName("dropicon")[0].style.display = "none", dropzoneId.classList.add("dropzone_sm"), dropzoneId.classList.remove("dropzone_lg"))
+      : (document.getElementsByClassName("dropicon")[0].style.display = "block", dropzoneId.classList.add("dropzone_lg"), dropzoneId.classList.remove("dropzone_sm"));
   });
   observer.observe(list, config);
+}
+
+function customDrag() {
+
+  const draggables = document.querySelectorAll('.optioncontainer')
+  const containers = document.querySelectorAll('#right_options')
+  const dropzones = document.querySelectorAll('#dropzone')
+
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', event.target.id);
+      draggable.classList.add('dragging')
+      event.dataTransfer.effectAllowed = "move";
+    })
+
+    draggable.addEventListener('dragend', (e) => {
+      draggable.classList.remove('dragging')
+    })
+  })
+
+  containers.forEach(container => {
+    let afterElement;
+    container.addEventListener('dragover', e => {
+      e.preventDefault()
+    })
+
+    container.addEventListener('drop', e => {
+      createOptions();
+      const draggable = document.querySelector('.dragging')
+      const afterElement = getDragAfterElement(container, e.clientY)
+      if (afterElement == null) {
+        container.appendChild(draggable)
+      } else {
+        container.insertBefore(draggable, afterElement)
+      }
+    });
+  })
+
+  dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragover', e => {
+      e.preventDefault()
+    })
+
+    dropzone.addEventListener('drop', event => {
+      createOptions();
+
+      const id = event.dataTransfer.getData('text');
+      const draggableElement = document.getElementById(id);
+      const droparea = document.getElementById("right_options")
+      droparea.prepend(draggableElement);
+      event.dataTransfer.clearData();
+    })
+  })
+
+  function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+  }
 }
