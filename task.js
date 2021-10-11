8cc1a94a3ca06a979cc027b41bc6ea9b0254bd71
gokul <you@example.com>
2021-10-11 12:27:29 +0530

    j

--- task.js
+++ task.js
@@ -5,14 +5,13 @@ document.addEventListener(
   "DOMContentLoaded",
   function () {
     createOptions();
-    getSelectedList();
+    customDrag();
   },
   false
 );
 
 function createOptions() {
-
-  var searchOption = document.getElementById("left_searchbox").value.toUpperCase();
+  let searchOption = document.getElementById("left_searchbox").value.toUpperCase();
   const name = document.getElementsByClassName("left_options")[0];
   name.innerHTML = "";
 
@@ -21,89 +20,24 @@ function createOptions() {
     options.sort().forEach(function (value, i) {
       optionBox(value, i);
     });
-
   }
   else {
-
     const curentvalue = options.filter(val => val.toUpperCase().indexOf(searchOption.trim()) > -1)
     if (curentvalue.length) {
-
       curentvalue.forEach(function (value, i) {
         optionBox(value, i);
       });
-    } else {
-
+    }
+    else {
       let display = `<div class="nodata"><i class="fas fa-search fa-8x"></i><p>Search Not Found</p></div>`;
       name.innerHTML = display
     }
   }
-  customDrag();
+  setEventOnDraggable();
 }
 
-function optionBox(value, i) {
-
-  const id = value.split(" ").join("").replace(/[^0-9a-z]/gi, '');
-  const name = document.getElementById("left_options");
-  let div = document.createElement('div');
-
-  div.setAttribute('id', `optionbox${id}`);
-  div.setAttribute('class', "draggable optioncontainer")
-  div.innerHTML = ` <div  class='optionbox'><p>${value}</p><i class="fas fa-grip-vertical" ></i></div> <i class="fas fa-trash-alt fa-lg deletebtn" onclick='deleteoptions("optionbox${id}","${value}")'></i> `;
-  div.setAttribute('draggable', true)
-  name.appendChild(div);
-}
-
-function deleteoptions(id, value) {
-
-  myobj = document.querySelectorAll(`#${id}`)[0];
-  myobj.parentNode.removeChild(myobj);
-  options.push(value)
-  createOptions();
-  customDrag();
-}
-
-function getSelectedList() {
-  const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-  const list = document.getElementById("right_options")
-
-  const config = {
-    attributes: true,
-    childList: true,
-    characterData: true,
-    subtree: true,
-  };
-
-  const observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      if (mutation.type === "childList") {
-        let list_values = [];
-        list_values = [].slice.call(list.childNodes).map(function (node) {
-          return node.getElementsByTagName('p')[0].innerHTML;
-        })
-        selectedoptions = list_values
-        list_values.forEach(val => {
-          let index = options.indexOf(val);
-          if (index > -1) {
-            options.splice(index, 1);
-          }
-        })
-      }
-    });
-
-    const dropzoneId = document.getElementById("dropzone");
-    selectedoptions.length > 0 ?
-      (document.getElementsByClassName("dropicon")[0].style.display = "none", dropzoneId.classList.add("dropzone_sm"), dropzoneId.classList.remove("dropzone_lg"))
-      : (document.getElementsByClassName("dropicon")[0].style.display = "block", dropzoneId.classList.add("dropzone_lg"), dropzoneId.classList.remove("dropzone_sm"));
-  });
-  observer.observe(list, config);
-}
-
-function customDrag() {
-
+function setEventOnDraggable() {
   const draggables = document.querySelectorAll('.optioncontainer')
-  const containers = document.querySelectorAll('#right_options')
-  const dropzones = document.querySelectorAll('#dropzone')
-
 
   draggables.forEach(draggable => {
     draggable.addEventListener('dragstart', (event) => {
@@ -116,7 +50,54 @@ function customDrag() {
       draggable.classList.remove('dragging')
     })
   })
+}
 
+function optionBox(value, i) {
+  const id = value.split(" ").join("").replace(/[^0-9a-z]/gi, '');
+  const name = document.getElementById("left_options");
+  let div = document.createElement('div');
+
+  div.setAttribute('id', `optionbox${id}`);
+  div.setAttribute('class', "draggable optioncontainer")
+  div.innerHTML = ` <div  class='optionbox'><p>${value}</p><i class="fas fa-grip-vertical" ></i></div> <i class="fas fa-trash-alt fa-lg deletebtn" onclick='deleteoptions("optionbox${id}","${value}")'></i> `;
+  div.setAttribute('draggable', true)
+  name.appendChild(div);
+}
+
+function deleteoptions(id, value) {
+  myobj = document.querySelectorAll(`#${id}`)[0];
+  myobj.parentNode.removeChild(myobj);
+  options.push(value)
+  createOptions();
+  getSelectedList();
+
+}
+
+function getSelectedList() {
+  let searchOption = document.getElementById("left_searchbox").value.toUpperCase();
+  const list = document.getElementById("right_options")
+  const dropzoneId = document.getElementById("dropzone");
+
+  list_values = [].slice.call(list.childNodes).map(function (node) {
+    return node.getElementsByTagName('p')[0].innerHTML;
+  })
+  selectedoptions = list_values
+  list_values.forEach(val => {
+    let index = options.indexOf(val);
+    if (index > -1) {
+      options.splice(index, 1);
+    }
+  });
+  selectedoptions.length > 0 ?
+    (document.getElementsByClassName("dropicon")[0].style.display = "none", dropzoneId.classList.add("dropzone_sm"), dropzoneId.classList.remove("dropzone_lg")) :
+    (document.getElementsByClassName("dropicon")[0].style.display = "block", dropzoneId.classList.add("dropzone_lg"), dropzoneId.classList.remove("dropzone_sm"));
+  searchOption ? createOptions() : "";
+
+}
+
+function customDrag() {
+  const containers = document.querySelectorAll('#right_options')
+  const dropzones = document.querySelectorAll('#dropzone')
   containers.forEach(container => {
     let afterElement;
     container.addEventListener('dragover', e => {
@@ -124,30 +105,30 @@ function customDrag() {
     })
 
     container.addEventListener('drop', e => {
-      createOptions();
       const draggable = document.querySelector('.dragging')
       const afterElement = getDragAfterElement(container, e.clientY)
       if (afterElement == null) {
         container.appendChild(draggable)
-      } else {
+      }
+      else {
         container.insertBefore(draggable, afterElement)
       }
+      getSelectedList();
+
     });
   })
-
   dropzones.forEach(dropzone => {
     dropzone.addEventListener('dragover', e => {
       e.preventDefault()
     })
 
     dropzone.addEventListener('drop', event => {
-      createOptions();
-
       const id = event.dataTransfer.getData('text');
       const draggableElement = document.getElementById(id);
       const droparea = document.getElementById("right_options")
       droparea.prepend(draggableElement);
       event.dataTransfer.clearData();
+      getSelectedList();
     })
   })
 
